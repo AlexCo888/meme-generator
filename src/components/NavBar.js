6cfb1715f657d5e1806396c3f9af3ceded4b3c4f
Alexco888 <contacto@cafedelossentidos.com>
2022-01-29 17:38:47 -0600

    First commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <h1>⚡ Meme Generator</h1>
+        <h3>React Mini Project</h3>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
